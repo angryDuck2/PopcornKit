70d04bf34da0b24f92ed636a195b97c471b37dd6
User <none@yoogle.com>
2016-08-24 02:55:00 +0300

    stability fixes

--- PopcornKit/Models/Episode.swift
+++ PopcornKit/Models/Episode.swift
@@ -2,7 +2,7 @@ import Foundation
 import ObjectMapper
 
 public struct Episode: Mappable, Equatable {
-
+    
     public var dateBased: Bool!
     public var firstAirDate: NSDate!
     public var overview: String!
@@ -11,22 +11,38 @@ public struct Episode: Mappable, Equatable {
     public var episode: Int!
     public var torrents: [Torrent]!
     public var tvdbId: Int!
-
+    
     public init(_ map: Map) {
-
+        
     }
-
+    
     public mutating func mapping(map: Map) {
         self.dateBased <- map["date_based"]
         self.firstAirDate <- (map["first_aired"], DateTransform())
         self.overview <- map["overview"]
         self.title <- map["title"]
-        self.season <- map["season"]
-        self.episode <- map["episode"]
+        var temp: String!;
+        temp <- map["season"]
+        if temp != nil{
+            self.season = NSNumberFormatter().numberFromString(temp)?.integerValue
+        }else{
+            self.season <- map["season"]
+        }
+        temp <- map["episode"]
+        if temp != nil{
+            self.episode = NSNumberFormatter().numberFromString(temp)?.integerValue
+        }else{
+            self.episode <- map["episode"]
+        }
         self.torrents <- map["torrents"]
-        self.tvdbId <- map["tvdb_id"]
+        temp <- map["tvdb_id"]
+        if temp != nil{
+            self.tvdbId = NSNumberFormatter().numberFromString(temp)?.integerValue
+        }else{
+            self.tvdbId <- map["tvdb_id"]
+        }
     }
-
+    
 }
 
 public func == (lhs: Episode, rhs: Episode) -> Bool {
